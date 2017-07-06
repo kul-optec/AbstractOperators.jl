013aedc2f38a63c354719253b2a95c7701e8bac4
nantonel <nantonel@esat.kuleuven.be>
2017-07-06 15:11:50 +0200

    generalized variation for all arrays sizes

--- src/operators/Variation.jl
+++ src/operators/Variation.jl
@@ -7,7 +7,6 @@ end
 # Constructors
 #default constructor
 function Variation{N}(domainType::Type, dim_in::NTuple{N,Int}) 
-	N > 3 && error("Variation is currently implemented only for Arrays of ndims of 2 or 3")
 	N == 1 && error("use FiniteDiff instead!")
 	Variation{domainType,N}(dim_in)
 end
@@ -19,62 +18,55 @@ Variation(x::AbstractArray)  = Variation(eltype(x), size(x))
 
 # Mappings
 
-function A_mul_B!{T}(y::AbstractArray{T,2}, A::Variation{T,2}, b::AbstractArray{T,2})
-	cnt = 0
-	for m = 1:size(b,2), l = 1:size(b,1)
-		cnt += 1
-		y[cnt,1] = l == 1 ? b[l+1,m]-b[l,m] : b[l,m]-b[l-1,m]
-		y[cnt,2] = m == 1 ? b[l,m+1]-b[l,m] : b[l,m]-b[l,m-1]
+#TODO use @generated
+@generated function A_mul_B!{T,N}(y::AbstractArray{T,2}, A::Variation{T,N}, b::AbstractArray{T,N})
+
+	ex = :()
+
+	for i = 1:N
+		z = zeros(Int,N)
+		z[i] = 1
+		z = (z...)
+		ex = :($ex; y[cnt,$i] = I[$i] == 1 ? b[I+CartesianIndex($z)]-b[I] : 
+		       b[I]-b[I-CartesianIndex($z)])
+	end
+
+	ex2 = quote 
+		cnt = 0
+		for I in CartesianRange(size(b))
+			cnt += 1
+			$ex
+		end
+		return y
 	end
 end
 
-function Ac_mul_B!{T}(y::AbstractArray{T,2}, A::Variation{T,2}, b::AbstractArray{T,2})
+@generated function Ac_mul_B!{T,N}(y::AbstractArray{T,N}, A::Variation{T,N}, b::AbstractArray{T,2})
 
-	cnt = 0
-	Nx, Ny = size(y,1), size(y,2)
-	for m = 1:Ny, l = 1:Nx
-		cnt += 1
-		y[l,m] = ((
-	     l == 1  ? -(b[cnt,1] + b[cnt+1,1]) :
-	     l == 2  ?   b[cnt,1] + b[cnt-1,1] - b[cnt+1,1] :
-	     l == Nx ?   b[cnt,1] : b[cnt,1]   - b[cnt+1,1])
-	    +(
-       	     m == 1  ? -(b[cnt,2] + b[cnt+Nx,2]) :
-             m == 2  ?   b[cnt,2] + b[cnt-Nx,2] - b[cnt+Nx,2] :
-             m == Ny ?   b[cnt,2] : b[cnt,2]    - b[cnt+Nx,2] ))
-	end
-end
+	ex = :(y[I] = I[1] == 1  ? -(b[cnt,1] + b[cnt+1,1]) :
+	              I[1] == 2  ?   b[cnt,1] + b[cnt-1,1] - b[cnt+1,1] :
+	       I[1] == size(y,1) ?   b[cnt,1] : b[cnt,  1] - b[cnt+1,1]
+	       )
 
-function A_mul_B!{T}(y::AbstractArray{T,2},A::Variation{T,3},b::AbstractArray{T,3})
-	cnt = 0
-	for n = 1:size(b,3), m = 1:size(b,2), l = 1:size(b,1)
-		cnt += 1
-		y[cnt,1] = l == 1 ? b[l+1,m,n]-b[l,m,n] : b[l,m,n]-b[l-1,m,n]
-		y[cnt,2] = m == 1 ? b[l,m+1,n]-b[l,m,n] : b[l,m,n]-b[l,m-1,n]
-		y[cnt,3] = n == 1 ? b[l,m,n+1]-b[l,m,n] : b[l,m,n]-b[l,m,n-1]
-	end
-end
-
-function Ac_mul_B!{T}(y::AbstractArray{T,3},A::Variation{T,3},b::AbstractArray{T,2})
-	cnt = 0
-	Nx, Ny, Nz = size(y,1), size(y,2), size(y,3)
-	Nxy = Nx*Ny
-	for n = 1:Nz, m = 1:Ny, l = 1:Nx
-		cnt += 1
-		y[l,m,n] = ((
-	        l == 1  ? -(b[cnt,1] + b[cnt+1,1]) :
-	        l == 2  ?   b[cnt,1] + b[cnt-1,1] - b[cnt+1,1] :
-	        l == Nx ?   b[cnt,1] : b[cnt,1]   - b[cnt+1,1])
-	      +(
-	 	m == 1  ? -(b[cnt,2] + b[cnt+Nx,2]) :
-	 	m == 2  ?   b[cnt,2] + b[cnt-Nx,2] - b[cnt+Nx,2] :
-	 	m == Ny ?   b[cnt,2] : b[cnt,2]    - b[cnt+Nx,2] )
-	      +(
-	 	n == 1  ? -(b[cnt,3] + b[cnt+Nxy,3]) :
-	 	n == 2  ?   b[cnt,3] + b[cnt-Nxy,3] - b[cnt+Nxy,3] :
-	 	n == Nz ?   b[cnt,3] : b[cnt,3]     - b[cnt+Nxy,3] ))
+	Nx = :(size(y,1))
+	for i = 2:N
+		ex = quote 
+			$ex 
+			y[I] += I[$i] == 1  ? -(b[cnt,$i] + b[cnt+$Nx,$i]) :
+			        I[$i] == 2  ?   b[cnt,$i] + b[cnt-$Nx,$i] - b[cnt+$Nx,$i] :
+			        I[$i] == size(y,$i) ?   b[cnt,$i] : b[cnt,  $i]   - b[cnt+$Nx,$i]
+		end
+		Nx = :($Nx*size(y,$i))
 	end
 
+	ex2 = quote 
+		cnt = 0
+		for I in CartesianRange(size(y))
+			cnt += 1
+			$ex
+		end
+		return y
+	end
 end
 
 # Properties
