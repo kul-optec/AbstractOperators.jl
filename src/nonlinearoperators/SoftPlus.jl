72a8d6ea7e40d8acce8e15c212a17a84c3abbc46
nantonel <nantonel@esat.kuleuven.be>
2017-11-24 20:45:35 +0100

    added SoftPlus

---
+++ src/nonlinearoperators/SoftPlus.jl
@@ -0,0 +1,37 @@
+export SoftPlus
+
+"""
+`SoftPlus([domainType=Float64::Type,] dim_in::Tuple)`
+
+Creates the softplus non-linear operator with input dimensions `dim_in`.
+```math
+\\sigma(\\mathbf{x}) = \\log (1 + e^{x} )
+```
+
+"""
+struct SoftPlus{T,N} <: NonLinearOperator
+	dim::NTuple{N,Int}
+end
+
+function SoftPlus(DomainType::Type, DomainDim::NTuple{N,Int}) where {N} 
+	SoftPlus{DomainType,N}(DomainDim)
+end
+
+SoftPlus(DomainDim::NTuple{N,Int}) where {N} = SoftPlus{Float64,N}(DomainDim)
+
+function A_mul_B!(y::AbstractArray{T,N}, L::SoftPlus{T,N}, x::AbstractArray{T,N}) where {T,N}
+	y .= log.(1.+exp.(x))
+end
+
+function Ac_mul_B!(y::AbstractArray{T,N}, 
+		   L::Jacobian{A}, 
+		   b::AbstractArray{T,N}) where {T,N, A <: SoftPlus{T,N}}
+	y .= 1./(1.+exp.(-L.x)).*b
+end
+
+fun_name(L::SoftPlus) = "σ"
+
+size(L::SoftPlus) = (L.dim, L.dim)
+
+domainType(L::SoftPlus{T,N}) where {T,N} = T
+codomainType(L::SoftPlus{T,N}) where {T,N} = T
