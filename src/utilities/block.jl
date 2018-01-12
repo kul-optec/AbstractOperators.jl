5dceb203408b11ea21b3a1573f4750c5be7c8bfb
Lorenzo Stella <stellalo@amazon.com>
2018-01-13 00:17:32 +0100

    updated syntax, fixed blockcopy

--- src/utilities/block.jl
+++ src/utilities/block.jl
@@ -35,16 +35,16 @@ blocklength(x::Tuple) = sum(blocklength.(x))
 blocklength(x::AbstractArray) = length(x)
 
 blockvecnorm(x::Tuple) = sqrt(blockvecdot(x, x))
-blockvecnorm{R <: Number}(x::AbstractArray{R}) = vecnorm(x)
+blockvecnorm(x::AbstractArray{R}) where {R <: Number} = vecnorm(x)
 
 blockmaxabs(x::Tuple) = maximum(blockmaxabs.(x))
-blockmaxabs{R <: Number}(x::AbstractArray{R}) = maximum(abs, x)
+blockmaxabs(x::AbstractArray{R}) where {R <: Number}= maximum(abs, x)
 
 blocksimilar(x::Tuple) = blocksimilar.(x)
 blocksimilar(x::AbstractArray) = similar(x)
 
 blockcopy(x::Tuple) = blockcopy.(x)
-blockcopy(x::Array) = copy(x)
+blockcopy(x::AbstractArray) = copy(x)
 
 blockcopy!(y::Tuple, x::Tuple) = blockcopy!.(y, x)
 blockcopy!(y::AbstractArray, x::AbstractArray) = copy!(y, x)
@@ -52,8 +52,8 @@ blockcopy!(y::AbstractArray, x::AbstractArray) = copy!(y, x)
 blockset!(y::Tuple, x) = blockset!.(y, x)
 blockset!(y::AbstractArray, x) = (y .= x)
 
-blockvecdot{T <: Tuple}(x::T, y::T) = sum(blockvecdot.(x,y))
-blockvecdot{R <: Number}(x::AbstractArray{R}, y::AbstractArray{R}) = vecdot(x, y)
+blockvecdot(x::T, y::T) where {T <: Tuple} = sum(blockvecdot.(x,y))
+blockvecdot(x::AbstractArray{R}, y::AbstractArray{R}) where {R <: Number} = vecdot(x, y)
 
 blockzeros(t::Tuple, s::Tuple) = blockzeros.(t, s)
 blockzeros(t::Type, n::NTuple{N, Integer} where {N}) = zeros(t, n)
