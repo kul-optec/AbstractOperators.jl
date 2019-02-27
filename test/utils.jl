fe457a8cdd5088eaabab90e7dc81265fc4d019ee
Lorenzo Stella <lostella@users.noreply.github.com>
2019-02-27 09:58:49 +0100

    Update test/utils.jl
    Co-Authored-By: nantonel <nantonel@esat.kuleuven.be>

--- test/utils.jl
+++ test/utils.jl
@@ -134,7 +134,7 @@ function gradient_fd(op::A,
                      y0::ArrayPartition, 
                      x0::ArrayPartition, 
                      r::ArrayPartition) where {A<:AbstractOperator} 
-	grad = 0 .*similar(x0)
+	grad = zero(x0)
 	y    = 0 .*similar(y0)
   M = length(x0.x)
   N = length(y0.x)
